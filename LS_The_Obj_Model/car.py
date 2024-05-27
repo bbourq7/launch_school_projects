0115b722d84ba23d653ede076bb5347837a66673
Ben Bourque <bbourq7@gmail.com>
2024-05-27 16:11:31 -0400

    added car.py for the 'Classes and Objects' exercise..

--- LS_The_Obj_Model/car.py
+++ LS_The_Obj_Model/car.py
@@ -5,6 +5,8 @@ class Car:
 		self.year = year
 		self.color = color
 		self.speed = 0
+		self.acceleration = 0
+		self.brake_power = 1
 		self.state = 'Off'
 		print(f'Thanks for entering your {color} {year} {model} in to the class object!')
 		print(f'You are going {self.speed} MPH')
@@ -16,5 +18,40 @@ class Car:
 		self.state = 'On'
 		print (f'Vroooom!!')
 		self.is_on_off()
+
+	def accelerate(self):
+		if self.state == 'Off':
+			print(f"Your car is {self.state}, you can't accelerate..")
+		else:
+			self.acceleration += 1
+			self.speed += self.acceleration
+			print(f'Your current acceleration is: {self.acceleration}')
+			print(f'Your current speed is {self.speed}')
+
+	def brake(self):
+		if self.speed == 0:
+			print(f'Your speed is {self.speed}. Braking did nothing..')
+		if self.acceleration >= 1:
+			self.acceleration = 0
+			print(f'Your acceleration is now {self.acceleration}')
+		if self.speed > 0:
+			self.speed -= self.brake_power
+			if self.speed < 0:
+				self.speed = 0
+			print(f'Your current speed is {self.speed}')
+		
+	def check_speed(self):
+		print(f'Your current speed is {self.speed} MPH.')
+
+	def stop(self):
+		if self.state == 'Off':
+			print(f"Your car is already {self.state}, you can't turn it off again..")
+		else:
+			self.state = 'Off'
+			print(f'<crickets>')
+			self.is_on_off()
+			self.speed = 0
+
+		
                 
 	
