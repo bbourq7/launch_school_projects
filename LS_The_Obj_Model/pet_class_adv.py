a904a5a04db0d92a5950f7d4bdc52d3fd498655c
Ben Bourque <bbourq7@gmail.com>
2024-05-27 11:53:07 -0400

    Further work on classes; added good_dog.py and tested additional class functionality

--- LS_The_Obj_Model/pet_class_adv.py
+++ LS_The_Obj_Model/pet_class_adv.py
@@ -1,14 +1,17 @@
 class GoodDog:
 
 	def __init__ (self, name, age):
-		self.name = name
-		self.age = age
+		self._name = name
+		self._age = age
 		
 	def speak(self):
 		return f'{self._name} says arf!'
+	
+	def name(self):
+		return self._name
 
-	def set_name(self, new name):
-		if not isintance(new_name, str):
+	def set_name(self, new_name):
+		if not isinstance(new_name, str):
 			raise TypeError('Name must be a string')
 		self._name = new_name
 
