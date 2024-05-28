7ea497a551f7566b26289e48b1b0afe2c9d96450
Ben Bourque <bbourq7@gmail.com>
2024-05-28 10:33:42 -0400

    working through vehicle.py in Inheritance chapter

--- LS_The_Obj_Model/Inheritance/vehicle.py
+++ LS_The_Obj_Model/Inheritance/vehicle.py
@@ -1,10 +1,21 @@
 class Vehicle:
 
+    def __init__(self, wheels):
+        self.wheels = wheels
+        print(f'I have {self.wheels} wheels.')
+
     def stop(self):
         print('I am stopped')
 
+    def drive(self):
+        print('I am driving')
+
 
 class Car(Vehicle):
+
+    def __init__(self):
+        print('Creating a car')
+        super().__init__(4)
 
     def drive(self):
         print('I am driving.')
@@ -14,8 +25,23 @@ class Car(Vehicle):
 
 class Truck(Vehicle):
 
+    def __init__(self):
+        print('Creating a Truck')
+        super().__init__(18)
+
     def drive(self):
         print('I am driving')
 
     def brake(self):
         super().stop()
+
+class Motorcycle(Vehicle):
+
+    def __init__(self):
+        print('Creating a motorcycle.')
+        super().__init__(2)
+
+    def drive(self):
+        super().drive()
+        print('   No! I am riding')
+
